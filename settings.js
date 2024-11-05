ffcc561f62881d5dae56e7086b0b7a150bd9a214
Bot-RonyMD053 <tatut0029@gmail.com>
2024-11-05 12:06:14 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,18 +10,18 @@ global.location = "India, West Bengal, Darjeeling" //ur location
 
 //new
 global.botname = 'Zenitsu-Bot' //ur bot name
-global.ownernumber = ['917029257330'] //ur owner number, dont add more than one
+global.ownernumber = ['+50374349804'] //ur owner number, dont add more than one
 global.ownername = '🦄Dream Guy Deepak' //ur owner name
 global.websitex = "https://youtube.com/@dreamguydeepak"
-global.wagc = "https://whatsapp.com/channel/0029VaDEDUaC6ZvewFKv6835"
+global.wagc = "https://chat.whatsapp.com/I7WiWv6cuLJE6x3DiThgzx"
 global.themeemoji = '🦄'
 global.wm = "Deepak Bot Inc."
 global.botscript = 'https://github.com/DGDEEPAK/Zenitsu-Bot9' //script link
 global.packname = "Zenitsu-Bot"
 global.author = "🦄Dream Guy Deepak"
-global.creator = "917029257330@s.whatsapp.net"
+global.creator = "50374349804@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["917029257330"] // Premium User
+global.premium = ["+50374349804"] // Premium User
 
 //channel id
 global.xchannel = {
